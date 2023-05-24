93deeeb9202341b91e34c1c5043924357a3f090d
Miguel Usma <usmamiguel04@gmail.com>
2023-05-23 23:40:59 -0500

    Service Model

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,7 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore; 
+﻿using Examen2.Models;
+using Microsoft.EntityFrameworkCore; 
 namespace Examen2.Data
 {
-    public class DbContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+
+        public DbSet<User> User { get; set; }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Examen2.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Examen2
 {
     public class Program
@@ -8,6 +11,10 @@ namespace Examen2
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("RostrosDB"))
+            );
 
             var app = builder.Build();
 
