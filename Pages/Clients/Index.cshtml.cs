01a560a62f04f47f3e1e634c2ea5f7d9cbb95844
Miguel Usma <usmamiguel04@gmail.com>
2023-05-24 12:55:36 -0500

    Index Client

---
+++ Pages/Clients/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Examen2.Data;
+using Examen2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examen2.Pages.Clients
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DataContext _context;
+        public IndexModel(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Client> Clients { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Clients != null)
+            {
+                Clients = await _context.Clients.ToListAsync();
+            }
+        }
+    }
+}
+  
