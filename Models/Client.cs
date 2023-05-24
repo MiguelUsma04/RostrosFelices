cc28dd327e43005b2291d1936b356b3868cb3c0d
Miguel Usma <usmamiguel04@gmail.com>
2023-05-23 23:16:50 -0500

    Invoice Model

---
+++ Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace Examen2.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Document_Number { get; set; }
+
+    }
+}
---
+++ Models/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Examen2.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int EmployeeId { get; set; }
+        public int ClientId { get; set; }
+        public int ServiceId { get; set; }
+        public ICollection<Employee>? Employees { get; set; } = default!;
+    }
+}
