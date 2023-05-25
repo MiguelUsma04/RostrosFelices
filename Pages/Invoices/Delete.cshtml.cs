ce71b85547f50062d14c5c9fb3f7dbeaa7e7939f
Miguel Usma <usmamiguel04@gmail.com>
2023-05-25 00:02:54 -0500

    Delete Invoice

---
+++ Pages/Invoices/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+using Examen2.Data;
+using Examen2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examen2.Pages.Invoices
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly DataContext _context;
+        public DeleteModel(DataContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Invoice Invoice { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Invoices == null)
+            {
+                return NotFound();
+            }
+            var detail = await _context.Invoices.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (detail == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Invoice = detail;
+            }
+            return Page();
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Invoices == null)
+            {
+                return NotFound();
+            }
+            var detail = await _context.Invoices.FindAsync(id);
+
+            if (detail != null)
+            {
+                Invoice = detail;
+                _context.Invoices.Remove(Invoice);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("./Index");
+        }
+
+
+    }
+}
