b45d7242668cab3ae18c0c2b79c48c43383068f8
Miguel Usma <usmamiguel04@gmail.com>
2023-05-24 23:53:08 -0500

    Index Invoice

---
+++ Pages/Invoices/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using Examen2.Data;
+using Examen2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examen2.Pages.Invoices
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DataContext _context;
+        public IndexModel(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Invoice> Invoices { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Invoices != null)
+            {
+                Invoices = await _context.Invoices.ToListAsync();
+            }
+        }
+    }
+}
